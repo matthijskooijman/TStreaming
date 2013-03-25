1dec243ef56d2061db00007f4217ccbebfd707a3
Matthijs Kooijman <matthijs@stdin.nl>
2013-03-25 16:45:35 +0100

    Optimize DoublePrint by skipping Print methods
    Instead of constructing a DoublePrint object and calling Print methods,
    which virtually call write(char) on DoublePrint again, the Print methods
    are reimplemented in DoublePrint, allowing the virtual call and, in most
    cases, the DoublePrint object construction to be optimized away.

--- DoublePrint.h
+++ DoublePrint.h
@@ -1,6 +1,8 @@
 #ifndef __TSTREAMING_DOUBLE_RINT_H
 #define __TSTREAMING_DOUBLE_PRINT_H
 
+#include "Types.h"
+
 /*
  * A printer that prints to two slave printers simultaneously.
  */
@@ -16,6 +18,44 @@ public:
    * our parent class. */
   virtual size_t write(uint8_t c) {
     return (a.write(c) + b.write(c)) / 2;
+  }
+
+  /* Optimization: Re-implement common Print methods, so the compiler
+   * can optimize away the DoublePrint constructor and the below print
+   * methods and compile
+   *   (Foo & Bar) << "baz";
+   * as if it were:
+   *   Foo.print("baz");
+   *   Bar.print("baz");
+   *
+   * Note that this produces slightly different behaviour than without
+   * these methods.  Now, the print methods are called twice (including
+   * any output conversion, e.g, formatting a hex number). Without the
+   * below methods, the print methods are called just once and the
+   * write(char) method is called twice for every character.
+   *
+   * This means that adding the below methods makes the code smaller,
+   * but (in a lot of cases) slower.
+   *
+   * Templates are used to easily re-implement all methods.
+   * */
+  template <typename T, typename enable_if<!is_base_of<Printable, T>::value, int>::type = 0>
+  size_t print(const T &v) {
+    return (a.print(v) + b.print(v)) / 2;
+  }
+
+  template <typename T1, typename T2>
+  size_t print(T1 v1, T2 v2) {
+    return (a.print(v1, v2) + b.print(v1, v2)) / 2;
+  }
+
+  /**
+   * Optimization: For Printable and subclasses, directly call printTo.
+   * This helps GCC to inline the printTo call in some cases.
+   */
+  template <typename T, typename enable_if<is_base_of<Printable, T>::value, int>::type = 0>
+  size_t print(const T &v) {
+    return (v.printTo(b) + v.printTo(a)) / 2;
   }
 
   /* Specialize the << operator for us, without using references. The
